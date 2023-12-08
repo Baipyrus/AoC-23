a70260cb8a25aa8a297f19348a5cf30b3ade0994
Baipyrus <simon.waltemathe@gmail.com>
2023-12-08 09:55:52 +0100

    d6p1 - read and parse input

--- Day6/Part1.js
+++ Day6/Part1.js
@@ -1,8 +1,16 @@
 import { readFileSync } from "fs";
 const t0 = performance.now();
 
-const input = readFileSync("input.txt").toString();
-console.log(input);
+const input = readFileSync("input.txt").toString().split("\n");
+const time = input[0]
+	.split(/\s{1,}/)
+	.slice(1)
+	.map(Number);
+const distance = input[1]
+	.split(/\s{1,}/)
+	.slice(1)
+	.map(Number);
+console.log(time, distance);
 
 const t1 = performance.now();
 console.log(`Runtime: ${t1 - t0}ms`);
