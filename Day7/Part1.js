167b259c9f41c715482deca9aed72368e7aa4c86
Baipyrus <simon.waltemathe@gmail.com>
2023-12-08 20:34:34 +0100

    d7p1 pattern lookup

--- Day7/Part1.js
+++ Day7/Part1.js
@@ -1,6 +1,15 @@
 import { readFileSync } from "fs";
 const t0 = performance.now();
 
+const patterns = [
+	"abcde",
+	"aabcd",
+	"aabbc",
+	"aaabc",
+	"aaabb",
+	"aaaab",
+	"aaaaa",
+];
 const input = readFileSync("input.txt").toString();
 console.log(input);
 
