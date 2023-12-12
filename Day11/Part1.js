49cbce92e9cdeeec70bb5b5e7c3b81e8ae0172f2
Baipyrus <simon.waltemathe@gmail.com>
2023-12-12 10:13:08 +0100

    douplicate rows

--- Day11/Part1.js
+++ Day11/Part1.js
@@ -1,8 +1,22 @@
 import { readFileSync } from "fs";
 const t0 = performance.now();
 
-const input = readFileSync("input.txt").toString();
-console.log(input);
+const input = readFileSync("input.txt")
+	.toString()
+	.split(/\r*\n/)
+	.filter((e) => e.length > 0)
+	.map((e) => e.split(""));
+
+const expand = (arr) =>
+	arr.reduce(
+		(a, i) => [
+			...a,
+			...(i.some((e) => e !== ".") ? [i] : [i, Array(i.length).fill(".")]),
+		],
+		[]
+	);
+
+console.log(expand(input));
 
 const t1 = performance.now();
 console.log(`Runtime: ${t1 - t0}ms`);
