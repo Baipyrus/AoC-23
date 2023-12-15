d6d3a06b85197e7fc20cb50862bf4b150af96273
Baipyrus <simon.waltemathe@gmail.com>
2023-12-15 20:38:30 +0100

    calc hashes, sum up

--- Day15/Part1.js
+++ Day15/Part1.js
@@ -1,8 +1,12 @@
 import { readFileSync } from "fs";
 const t0 = performance.now();
 
-const input = readFileSync("input.txt").toString();
-console.log(input);
+const input = readFileSync("input.txt").toString().split("\n")[0].split(",");
+const hashes = input.map((e) =>
+	e.split("").reduce((a, i) => ((a + i.charCodeAt(0)) * 17) % 256, 0)
+);
+const sum = hashes.reduce((a, i) => a + i);
+console.log(sum);
 
 const t1 = performance.now();
 console.log(`Runtime: ${t1 - t0}ms`);
