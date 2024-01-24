6c8ee04e9599980edb1f3c4442ff6003966db3ca
Baipyrus <simon.waltemathe@gmail.com>
2024-01-24 23:39:14 +0100

    runtime measurement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use std::time::Instant;
+
 mod common;
 mod day09;
 
 fn main() {
     println!("Executing main entrypoint . . . ");
+    let now = Instant::now();
+    
     day09::part_one();
+
+    let elapsed = now.elapsed();
+    println!("Execution in {:?}.", elapsed);
 }
