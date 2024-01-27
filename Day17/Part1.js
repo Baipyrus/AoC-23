00e53cdbd72692ef467eeb5fd9bd0b4710f38f46
Baipyrus <simon.waltemathe@gmail.com>
2024-01-27 22:17:21 +0100

    parse tiles into grid

--- Day17/Part1.js
+++ Day17/Part1.js
@@ -2,8 +2,19 @@ import { readFileSync } from "fs";
 const t0 = performance.now();
 
 const input = readFileSync("input.txt")
-    .toString();
-console.log(input);
+    .toString()
+    .split(/\r*\n/)
+    .filter((e) => e.length > 0)
+    .map((a, j) => a
+        .split("")
+        .map((b, i) => ({
+            c: +b,
+            x: i,
+            y: j,
+            d: 0,
+            h: 0
+        })));
+console.log(input[0].slice(-1)[0]);
 
 const t1 = performance.now();
 console.log(`Runtime: ${t1 - t0}ms`);
